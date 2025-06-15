eb6d1c2b488b12290df5350d01424c5a3a074e68
YangSpring429 <3424968114@qq.com>
2025-06-15 20:26:52 +0800

    feat：添加 MurmurHash2 算法

---
+++ WonderLab/Utilities/HashUtil.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace WonderLab.Utilities;
+
+public static class HashUtil {
+    public static uint GetFileMurmurHash2(string filePath) {
+        byte[] data = File.ReadAllBytes(filePath);
+        ReadOnlySpan<byte> filteredData = FilterBytes(data.AsSpan());
+        return ComputeMurmurHash2(filteredData);
+    }
+
+    private static ReadOnlySpan<byte> FilterBytes(Span<byte> data) {
+        int writeIndex = 0;
+        for (int i = 0; i < data.Length; i++) {
+            byte b = data[i];
+            if (b != 9 && b != 10 && b != 13 && b != 32)
+                Unsafe.WriteUnaligned(ref data[writeIndex++], b);
+        }
+
+        return data[..writeIndex];
+    }
+
+    private static uint ComputeMurmurHash2(ReadOnlySpan<byte> data) {
+        int length = data.Length;
+        uint h = 1 ^ (uint)length;
+        int i;
+
+        for (i = 0; i <= length - 4; i += 4) {
+            uint k = MemoryMarshal.Read<uint>(data.Slice(i, 4));
+            k *= 0x5BD1E995;
+            k ^= k >> 24;
+            k *= 0x5BD1E995;
+            h *= 0x5BD1E995;
+            h ^= k;
+        }
+
+        if (length > i) {
+            Span<byte> tail = stackalloc byte[4];
+            data[i..].CopyTo(tail);
+            uint remaining = MemoryMarshal.Read<uint>(tail);
+            h ^= remaining;
+            h *= 0x5BD1E995;
+        }
+
+        h ^= h >> 13;
+        h *= 0x5BD1E995;
+        h ^= h >> 15;
+
+        return h;
+    }
+}
