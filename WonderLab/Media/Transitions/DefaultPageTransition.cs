71db6746cae85b7b05950d9a20d7a514c3106e74
YangSpring429 <3424968114@qq.com>
2025-08-02 20:27:30 +0800

    feat：优化动画

--- WonderLab/Media/Transitions/DefaultPageTransition.cs
+++ WonderLab/Media/Transitions/DefaultPageTransition.cs
@@ -14,7 +14,7 @@ namespace WonderLab.Media.Transitions;
 
 public sealed class DefaultPageTransition : IPageTransition {
     public TimeSpan Duration { get; set; }
-    public Easing Easing { get; set; } = new CircularEaseInOut();
+    public Easing Easing { get; set; } = new ExponentialEaseOut();
 
     public DefaultPageTransition() { }
 
@@ -27,8 +27,8 @@ public sealed class DefaultPageTransition : IPageTransition {
             var fromEV = ElementComposition.GetElementVisual(from);
             var size = fromEV!.Size;
 
-            var opacityAni = CompositionAnimationUtil.CreateScalarAnimation(fromEV, 1, 0, Duration, Easing);
-            var scaleAni = CompositionAnimationUtil.CreateVector3Animation(fromEV, new(1), new(0.95f), Duration, Easing);
+            var opacityAni = CompositionAnimationUtil.CreateScalarAnimation(fromEV, 1, 0, TimeSpan.FromMilliseconds(400), Easing);
+            var scaleAni = CompositionAnimationUtil.CreateVector3Animation(fromEV, new(1), new(0.9f), Duration, Easing);
 
             scaleAni.Target = CompositionAnimationUtil.PROPERTY_SCALE;
             opacityAni.Target = CompositionAnimationUtil.PROPERTY_OPACITY;
@@ -46,7 +46,7 @@ public sealed class DefaultPageTransition : IPageTransition {
             var size = toEV!.Size;
 
             var opacityAni = CompositionAnimationUtil.CreateScalarAnimation(toEV, 0, 1, Duration, Easing);
-            var scaleAni = CompositionAnimationUtil.CreateVector3Animation(toEV, new(0.95f), new(1), Duration, Easing);
+            var scaleAni = CompositionAnimationUtil.CreateVector3Animation(toEV, new(1.2f), new(1), Duration, Easing);
 
             scaleAni.Target = CompositionAnimationUtil.PROPERTY_SCALE;
             opacityAni.Target = CompositionAnimationUtil.PROPERTY_OPACITY;
