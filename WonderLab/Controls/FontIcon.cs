8fba3afbe7277b96475ffb76a00028a6737bee0b
YangSpring429 <3424968114@qq.com>
2025-08-18 23:11:46 +0800

    feat：更新图标资源

--- WonderLab/Controls/FontIcon.cs
+++
@@ -1,9 +0,0 @@
-﻿using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Controls.Primitives;
-using WonderLab.SourceGenerator.Attributes;
-
-namespace WonderLab.Controls;
-
-[StyledProperty(typeof(string), "Glyph", "")]
-public sealed partial class FontIcon : TemplatedControl;
