b5bd79613becd72fd0e1fc3e2b697c7f36d93396
Ktn429 <3424968114@qq.com>
2026-07-18 22:43:26 +0800

    fix：修复标题栏相关的的 Bug

---
+++ WonderLab/UI/Controls/MaterialWindow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Primitives;
+using DialogHostAvalonia;
+
+namespace WonderLab.UI.Controls;
+
+[TemplatePart(Name = "PART_CloseButton", Type = typeof(Button), IsRequired = true)]
+[TemplatePart(Name = "PART_MinimizeButton", Type = typeof(Button), IsRequired = true)]
+[TemplatePart(Name = "PART_DialogHost", Type = typeof(DialogHost))]
+public class MaterialWindow : Window {
+    private Border _PART_TitleBarLayout;
+    private Button _PART_CloseButton;
+    private Button _PART_MinimizeButton;
+
+    protected override Type StyleKeyOverride => typeof(MaterialWindow);
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e) {
+        _PART_CloseButton = e.NameScope.Find<Button>("PART_CloseButton");
+        _PART_MinimizeButton = e.NameScope.Find<Button>("PART_MinimizeButton");
+        _PART_TitleBarLayout = e.NameScope.Find<Border>("PART_TitleBarLayout");
+
+        _PART_TitleBarLayout?.PointerPressed += (_, args) => BeginMoveDrag(args);
+
+        _PART_CloseButton?.Click += (_, _) => Close();
+        _PART_MinimizeButton?.Click += (_, _) => WindowState = WindowState.Minimized;
+    }
+}
